3643259374eed7bdeb2a8b9c4ae9da7dd52e379e
mSaico <m.saico@pucp.edu.pe>
2026-09-24 17:55:52 -0500

    Inyectar falla en main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -71,7 +71,7 @@ resource "aws_cloudwatch_log_group" "funcion" {
 }
 
 resource "aws_lambda_function" "app" {
-  function_name = var.nombre_aplicacion
+  function_name = "${var.nombre_aplicacion}-destruir"
   role          = local.arn_rol_ejecucion
   package_type  = "Image"
   image_uri     = local.imagen_inicial
